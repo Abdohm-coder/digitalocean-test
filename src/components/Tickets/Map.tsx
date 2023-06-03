1063639abd06b6a57438fb7901c331af4cf92a28
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-03 12:24:56 +0100

    update map " useEffect"

--- src/components/Tickets/Map.tsx
+++ src/components/Tickets/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsArrowsAngleContract, BsArrowsFullscreen } from "react-icons/bs";
 import MapImage from "@/assets/images/map.jpg";
 import { WBCID } from "@/settings/site.settings";
@@ -6,10 +6,21 @@ import Script from "next/script";
 
 const Map: React.FC<{ id: number }> = ({ id }) => {
   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://mapwidget3.seatics.com/js?eventId=${id}&websiteConfigId=${WBCID}`;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [id]);
   return (
     <>
       <Script
-        src={`https://mapwidget3.seatics.com/js?eventId=${id}&websiteConfigId=${WBCID}`}
+        src={`https://mapwidget3.seatics.com/js?eventId=${4312622}&websiteConfigId=${WBCID}`}
       />
       <div className="d-none d-lg-block">
         {/* <img src={MapImage} alt="Map" className="w-100" /> */}
