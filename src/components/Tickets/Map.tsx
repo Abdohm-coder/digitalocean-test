a93ed894a357cafe15c735e8633fc500c27652b1
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-03 12:18:10 +0100

    update Map script

--- src/components/Tickets/Map.tsx
+++ src/components/Tickets/Map.tsx
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BsArrowsAngleContract, BsArrowsFullscreen } from "react-icons/bs";
 import MapImage from "@/assets/images/map.jpg";
 import { WBCID } from "@/settings/site.settings";
+import Script from "next/script";
 
 const Map: React.FC<{ id: number }> = ({ id }) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://mapwidget3.seatics.com/js?eventId=${id}&websiteConfigId=${WBCID}`;
-    script.async = true;
-    document.body.appendChild(script);
-    // @ts-ignore
-    window.Seatics = window.Seatics || {};
-    // @ts-ignore
-    window.Seatics.Text = window.Seatics.Text || {};
-
-    // Adjust individual pieces of text
-    // @ts-ignore
-    window.Seatics.Text.DeepLink_CloseModalButton = "Select new tickets";
-    // @ts-ignore
-    window.Seatics.Text["Desktop_DisclosuresTextCurrency"] = function (text) {
-      return text.replace("USD", "Points");
-    };
-
-    // Clean up function when the component unmounts (optional)
-    // return () => {
-    //   document.body.removeChild(script);
-    //   delete window.Seatics.Text.DeepLink_CloseModalButton;
-    //   delete window.Seatics.Text["Desktop_DisclosuresTextCurrency"];
-    // };
-  }, [id]);
   const [show, setShow] = useState(false);
   return (
     <>
+      <Script
+        src={`https://mapwidget3.seatics.com/js?eventId=${id}&websiteConfigId=${WBCID}`}
+      />
       <div className="d-none d-lg-block">
         {/* <img src={MapImage} alt="Map" className="w-100" /> */}
       </div>
