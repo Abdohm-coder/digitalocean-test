9a7d9cdbb62c75d7e8fd44bc15090ac535efafed
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-03 22:49:06 +0100

    changes into Map and design

--- src/pages/tickets/[eventID].tsx
+++ src/pages/tickets/[eventID].tsx
@@ -71,15 +71,55 @@ export async function getServerSideProps(context) {
   const widgetHTML = response.data;
   const additionalScript = `
     <script>
-    Seatics.config.enableQuantityModal = true;
-    Seatics.config.mapContained = true;
-      Seatics.config.c3CheckoutDomain = "checkout.tickettransaction.com";
+      Seatics.config.enableQuantityModal = true;
+      Seatics.config.mapContained = true;
+      Seatics.config.c3CheckoutDomain = "checkout.ticketjewel.com";
       Seatics.config.c3CurrencyCode = "USD";
       Seatics.config.useC3 = true;
       Seatics.config.currencyIntl = {};
       Seatics.config.enableLegalDisclosureMobile = false;
       Seatics.config.enableHeaderLegalDisclosureMobile = true;
+                     Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
+               Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
+                Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
+                Seatics.config.tgGuaranteeNoteHtml = '';
+                Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
+Seatics.config.enableHeaderLowerLegalDisclosureMobile = false; 
+Seatics.config.smallScreenMapLayout =
+Seatics.SmallScreenMapOptions.FullyHidden;
+               Seatics.config.eventInfoLeftCol = '<a href="/"><img src=https://ticketiron.com/images/Tg_logo_small2xc.jpg height="55px" width="55px" style="padding:5px; vertical-align:top;" /></a>',
+               Seatics.config.eventInfoRightCol = '<a href="tel:8337832310"><span style="font-size:40px; vertical-align:top; padding:5px;" class="glyphicon glyphicon-earphone"></span></a>';
 
+ Seatics.config.mapFinishedRenderingHandler = function(){
+                    $('#tn-maps').addClass('completed');                
+                    Seatics.config.ticketSeparationOptions = {
+                      packages: Seatics.TicketGroupSeparationOptions.Separate,
+                      parking: Seatics.TicketGroupSeparationOptions.Separate,
+                      passes: Seatics.TicketGroupSeparationOptions.Separate,
+                      hotels: Seatics.TicketGroupSeparationOptions.Separate,
+                      unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
+                      unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
+                    };
+                
+                if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                    console.log("dsd");
+                    setTimeout(function(){ $("#list-ctn").css({ top: '96px' }); }, 900);
+                    
+}
+                  };
+                   $(document).ready(function(){
+        $(document).on('click','#map-shower',function(e){
+            e.pre
+            console.log("s");
+            if ( $(".sea-show-zoom-controls-mobile").css('display') == 'none' ){
+    $("#list-ctn").css({ top: '355px' });
+}else{
+    $("#list-ctn").css({ top: '96px' });
+}
+            $(".sea-show-zoom-controls-mobile").toggle();
+            
+        });
+    });
     </script>
   `;
 
