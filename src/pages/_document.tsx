619412df3b009679a98b6df9852e73c27a123018
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-05 07:56:05 +0100

    useEffect to add btn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,87 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const { query, pathname } = ctx; // Assuming the page ID is passed as a query parameter
+function Document () {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-    return { ...initialProps, pathname, eventID: query?.eventID };
-  }
-
-  render() {
-    const { eventID, pathname } = this.props;
-    console.log(eventID, pathname);
-
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, user-scalable=no, width=device-width"
-          />
-          {pathname?.includes("/tickets/") && (
-            <>
-              <Script
-                strategy="beforeInteractive"
-                src={`https://mapwidget3.seatics.com/js?eventId=${eventID}&websiteConfigId=4626`}
-                type="text/javascript"></Script>
-              <Script id="edit-map">
-                {`Seatics.config.c3CheckoutDomain = "checkout.ticketjewel.com";
-      Seatics.config.c3CurrencyCode = "USD";
-      Seatics.config.useC3 = true;
-      Seatics.config.currencyIntl = {};
-      
-               Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
-               Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
-			   Seatics.config.c3CheckoutDomain = "checkout.ticketfront.com";
-                Seatics.config.c3CurrencyCode = "USD";
-                Seatics.config.useC3 = true;
-                Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
-                Seatics.config.tgGuaranteeNoteHtml = '';
-                Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
-                Seatics.config.enableLegalDisclosureMobile = false;
-Seatics.config.enableHeaderLegalDisclosureMobile = false;
-Seatics.config.enableHeaderLowerLegalDisclosureMobile = false; 
-Seatics.config.smallScreenMapLayout =
-Seatics.SmallScreenMapOptions.FullyHidden;
-
-                Seatics.config.onBuyButtonClicked = function(){
-                //location.href = 'checkout.ticketgrow.com/Checkout/Order';
-                //_bShowCoupon = false;
-                
-              };
-              
-              Seatics.config.mapFinishedRenderingHandler = function(){
-                    document.querySelector('#tn-maps').classList.add('completed');                
-                    Seatics.config.ticketSeparationOptions = {
-                      packages: Seatics.TicketGroupSeparationOptions.Separate,
-                      parking: Seatics.TicketGroupSeparationOptions.Separate,
-                      passes: Seatics.TicketGroupSeparationOptions.Separate,
-                      hotels: Seatics.TicketGroupSeparationOptions.Separate,
-                      unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
-                      unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
-                    };
-                
-                if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                    console.log("dsd");
-                    setTimeout(function(){ document.querySelector("#list-ctn").style.top = '96px'; }, 900);
-                    
-}
-                  };
-		`}
-              </Script>
-            </>
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+export default Document;
--- src/pages/tickets/[eventID].tsx
+++ src/pages/tickets/[eventID].tsx
@@ -1,105 +1,110 @@
 // import { useState } from "react";
+import axios from "axios";
 // import { BsArrowsAngleContract, BsArrowsFullscreen } from "react-icons/bs";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { siteSettings } from "@/settings/site.settings";
-import Script from "next/script";
+import { useEffect } from "react";
 
-const TicketPage = ({ eventID }: { eventID: string }) => {
-  // const [show, setShow] = useState(false);
+const TicketPage = ({ widgetHTML }: { widgetHTML: string }) => {
+  useEffect(() => {
+    const addTableCell = () => {
+      const tableRow = document.querySelector(
+        "table.venue-ticket-list-tbl tbody tr"
+      );
+      if (tableRow) {
+        const tableData = document.createElement("td");
+        tableData.className =
+          "venue-ticket-list-cta-js venue-ticket-list-cta-col";
+
+        const button = document.createElement("button");
+        button.className = "btn-buy venue-ticket-list-checkout-trigger-js";
+        button.innerText = "Continue";
+
+        tableData.appendChild(button);
+        tableRow.appendChild(tableData);
+      }
+    };
+
+    addTableCell();
+  }, []);
   return (
     <div>
       <Head>
         <title>Map Widget | {siteSettings.site_name}</title>
       </Head>
-
-      {/* <div> */}
-      {/* <div dangerouslySetInnerHTML={{ __html: widgetHTML }} /> */}
-      {/* </div> */}
-      {/* <div className="d-block d-lg-none text-center">
-        {show && <div dangerouslySetInnerHTML={{ __html: widgetHTML }} />}
-        <button
-          className="btn btn-sm btn-light mt-2"
-          onClick={() => setShow((s) => !s)}>
-          {show ? "Hide Map " : "Show Map "}
-          {show ? <BsArrowsAngleContract /> : <BsArrowsFullscreen />}
-        </button>
-      </div> */}
-
-      {/* Render the widgetHTML */}
-
-      {/* Rest of your ticket results page content */}
+      <div dangerouslySetInnerHTML={{ __html: widgetHTML }} />
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const eventID = context.params?.eventID;
-  //   const WBCID = 4626;
-  //   const userAgent = context.headers?.["user-agent"] || "something";
+  const WBCID = 4626;
+  const userAgent = context.headers?.["user-agent"] || "something";
 
-  //   const response = await axios.get(
-  //     `https://mapwidget3.seatics.com/html?eventId=${eventID}&websiteConfigId=${WBCID}&userAgent=${userAgent}`
-  //   );
+  const response = await axios.get(
+    `https://mapwidget3.seatics.com/html?eventId=${eventID}&websiteConfigId=${WBCID}&userAgent=${userAgent}`
+  );
 
-  //   const widgetHTML = response.data;
-  //   const additionalScript = `
-  //     <script>
-  //       Seatics.config.c3CheckoutDomain = "checkout.ticketjewel.com";
-  //       Seatics.config.c3CurrencyCode = "USD";
-  //       Seatics.config.useC3 = true;
-  //       Seatics.config.currencyIntl = {};
+  const widgetHTML = response.data;
+  const additionalScript = `
+    <script>
+      Seatics.config.c3CheckoutDomain = "checkout.ticketjewel.com";
+      Seatics.config.c3CurrencyCode = "USD";
+      Seatics.config.useC3 = true;
+      Seatics.config.currencyIntl = {};
+      
+               Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
+               Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
+			   Seatics.config.c3CheckoutDomain = "checkout.ticketfront.com";
+                Seatics.config.c3CurrencyCode = "USD";
+                Seatics.config.useC3 = true;
+                Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
 
-  //                Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
-  //                Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
-  // 			   Seatics.config.c3CheckoutDomain = "checkout.ticketfront.com";
-  //                 Seatics.config.c3CurrencyCode = "USD";
-  //                 Seatics.config.useC3 = true;
-  //                 Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
-  //                 Seatics.config.tgGuaranteeNoteHtml = '';
-  //                 Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
-  //                 Seatics.config.enableLegalDisclosureMobile = false;
-  // Seatics.config.enableHeaderLegalDisclosureMobile = false;
-  // Seatics.config.enableHeaderLowerLegalDisclosureMobile = false;
-  // Seatics.config.smallScreenMapLayout =
-  // Seatics.SmallScreenMapOptions.FullyHidden;
+                Seatics.config.tgGuaranteeNoteHtml = '';
+                Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
+                Seatics.config.enableLegalDisclosureMobile = false;
+Seatics.config.enableHeaderLegalDisclosureMobile = false;
+Seatics.config.enableHeaderLowerLegalDisclosureMobile = false; 
+Seatics.config.smallScreenMapLayout =
+Seatics.SmallScreenMapOptions.FullyHidden;
 
-  //                 Seatics.config.onBuyButtonClicked = function(){
-  //                 //location.href = 'checkout.ticketgrow.com/Checkout/Order';
-  //                 //_bShowCoupon = false;
+                Seatics.config.onBuyButtonClicked = function(){
+                //location.href = 'checkout.ticketgrow.com/Checkout/Order';
+                //_bShowCoupon = false;
+                
+              };
+              
+              Seatics.config.mapFinishedRenderingHandler = function(){
+                    document.querySelector('#tn-maps').classList.add('completed');                
+                    Seatics.config.ticketSeparationOptions = {
+                      packages: Seatics.TicketGroupSeparationOptions.Separate,
+                      parking: Seatics.TicketGroupSeparationOptions.Separate,
+                      passes: Seatics.TicketGroupSeparationOptions.Separate,
+                      hotels: Seatics.TicketGroupSeparationOptions.Separate,
+                      unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
+                      unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
+                    };
+                
+                if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                    console.log("dsd");
+                    setTimeout(function(){ document.querySelector("#list-ctn").style.top = '96px'; }, 900);
+                    
+}
+                  };
+		
+    </script>
+  `;
 
-  //               };
-
-  //               Seatics.config.mapFinishedRenderingHandler = function(){
-  //                     document.querySelector('#tn-maps').classList.add('completed');
-  //                     Seatics.config.ticketSeparationOptions = {
-  //                       packages: Seatics.TicketGroupSeparationOptions.Separate,
-  //                       parking: Seatics.TicketGroupSeparationOptions.Separate,
-  //                       passes: Seatics.TicketGroupSeparationOptions.Separate,
-  //                       hotels: Seatics.TicketGroupSeparationOptions.Separate,
-  //                       unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
-  //                       unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
-  //                     };
-
-  //                 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  //                     console.log("dsd");
-  //                     setTimeout(function(){ document.querySelector("#list-ctn").style.top = '96px'; }, 900);
-
-  // }
-  //                   };
-
-  //     </script>
-  //   `;
-
-  //   const updatedWidgetHTML = widgetHTML.replace(
-  //     "</head>",
-  //     `${additionalScript}</head>`
-  //   );
+  const updatedWidgetHTML = widgetHTML.replace(
+    "</head>",
+    `${additionalScript}</head>`
+  );
 
   return {
     props: {
-      // widgetHTML: updatedWidgetHTML,
-      eventID,
+      widgetHTML: updatedWidgetHTML,
     },
   };
 };
