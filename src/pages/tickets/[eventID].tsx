ee8da6570530e922a23b6d6395c4bcec23c5e5c4
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-05 08:18:38 +0100

    debug

--- src/pages/tickets/[eventID].tsx
+++ src/pages/tickets/[eventID].tsx
@@ -54,46 +54,38 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       Seatics.config.c3CurrencyCode = "USD";
       Seatics.config.useC3 = true;
       Seatics.config.currencyIntl = {};
-      
-               Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
-               Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
-			   Seatics.config.c3CheckoutDomain = "checkout.ticketfront.com";
-                Seatics.config.c3CurrencyCode = "USD";
-                Seatics.config.useC3 = true;
-                Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
+      Seatics.config.skipPrecheckoutDesktop = true; /*default = false*/
+      Seatics.config.skipPrecheckoutMobile = true; /*default = false*/
+      Seatics.config.buyButtonContentHtml = '<button class="btn-buy venue-ticket-list-checkout-trigger-js">Continue</button>';
 
-                Seatics.config.tgGuaranteeNoteHtml = '';
-                Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
-                Seatics.config.enableLegalDisclosureMobile = false;
-Seatics.config.enableHeaderLegalDisclosureMobile = false;
-Seatics.config.enableHeaderLowerLegalDisclosureMobile = false; 
-Seatics.config.smallScreenMapLayout =
-Seatics.SmallScreenMapOptions.FullyHidden;
+      Seatics.config.tgGuaranteeNoteHtml = '';
+      Seatics.config.tgMarkTooltipText = 'These tickets are featured as a great value.';
+      Seatics.config.enableLegalDisclosureMobile = false;
+      Seatics.config.enableHeaderLegalDisclosureMobile = false;
+      Seatics.config.enableHeaderLowerLegalDisclosureMobile = false; 
+      Seatics.config.smallScreenMapLayout =
+      Seatics.SmallScreenMapOptions.FullyHidden;
 
-                Seatics.config.onBuyButtonClicked = function(){
-                //location.href = 'checkout.ticketgrow.com/Checkout/Order';
-                //_bShowCoupon = false;
-                
-              };
-              
-              Seatics.config.mapFinishedRenderingHandler = function(){
-                    document.querySelector('#tn-maps').classList.add('completed');                
-                    Seatics.config.ticketSeparationOptions = {
-                      packages: Seatics.TicketGroupSeparationOptions.Separate,
-                      parking: Seatics.TicketGroupSeparationOptions.Separate,
-                      passes: Seatics.TicketGroupSeparationOptions.Separate,
-                      hotels: Seatics.TicketGroupSeparationOptions.Separate,
-                      unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
-                      unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
-                    };
-                
-                if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                    console.log("dsd");
-                    setTimeout(function(){ document.querySelector("#list-ctn").style.top = '96px'; }, 900);
-                    
-}
-                  };
-		
+      Seatics.config.onBuyButtonClicked = function(){
+        //location.href = 'checkout.ticketgrow.com/Checkout/Order';
+        //_bShowCoupon = false;          
+      };       
+      Seatics.config.mapFinishedRenderingHandler = function(){
+        document.querySelector('#tn-maps').classList.add('completed');                
+        Seatics.config.ticketSeparationOptions = {
+          packages: Seatics.TicketGroupSeparationOptions.Separate,
+          parking: Seatics.TicketGroupSeparationOptions.Separate,
+          passes: Seatics.TicketGroupSeparationOptions.Separate,
+          hotels: Seatics.TicketGroupSeparationOptions.Separate,
+          unmappedFlex: Seatics.TicketGroupSeparationOptions.Separate,
+          unmappedStandard: Seatics.TicketGroupSeparationOptions.Separate
+        };    
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          console.log("dsd");
+          setTimeout(function(){ document.querySelector("#list-ctn").style.top = '96px'; }, 900);
+                      
+        }
+      };
     </script>
   `;
 
@@ -101,6 +93,7 @@ Seatics.SmallScreenMapOptions.FullyHidden;
     "</head>",
     `${additionalScript}</head>`
   );
+  console.log(updatedWidgetHTML);
 
   return {
     props: {
